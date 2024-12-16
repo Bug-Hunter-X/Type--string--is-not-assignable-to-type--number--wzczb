7185e21411f145f426ee517502fc7da049d40ae8
BugHunter-X <askzenai@gmail.com>
2024-12-17 00:56:06 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,18 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+let num1: number = 5;
+let num2: number = 3;
+let num3: number = 10;
+let num4: number = 5;
+
+let result1 = add(num1, num2); // result1 is 8
+let result2 = subtract(num3, num4); // result2 is 5
+
+console.log(result1); // Output: 8
+console.log(result2); // Output: 5
